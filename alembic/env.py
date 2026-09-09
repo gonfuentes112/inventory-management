00322f7a800b60690d4955343699633c971098ef
gonfuentes112 <gonfuentes112@gmail.com>
2026-09-09 17:33:32 +0900

    add db models

--- alembic/env.py
+++ alembic/env.py
@@ -3,6 +3,12 @@ from logging.config import fileConfig
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
+from app.db.database import Base
+from app.models.user import User
+from app.models.product import Product
+from app.models.category import Category
+
+
 from alembic import context
 
 # this is the Alembic Config object, which provides
@@ -18,7 +24,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
